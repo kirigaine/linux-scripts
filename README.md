cd8c8f18071e8b9bc9672e927489d23c5b4ab1b4
kirigaine <tholen2147@gmail.com>
2024-02-17 20:58:13 -0600

    Updated README.md and added more logic to easy_ufw.sh

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # linux-scripts
 Generic repository for scripts convenient for setting up my servers and other things
+
+## Overview
+
+## Prerequisites
+
+- Many of these scripts may require running the script as a root user or using `sudo`.
+
+## Usage
+
+A. Clone the repository
+	1a. Run the script
+
+B. Copy script to clipboard and paste into terminal. Run.
+
+# Disclaimer
+
+Use these scripts at your own risk. While I strive for quality work, mistakes are possible.  Make sure to review and understand the functions they propose before applying them to your system.
--- easy_ufw.sh
+++ easy_ufw.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-echo "***** EASY UFW - by: kirigaine *****"
+echo "***** EASY UFW --- by: kirigaine *****"
 if command -v ufw &> /dev/null; then
 
-	echo "Default denying incoming traffic"
+	echo "- Default denying incoming traffic"
 	#ufw default deny incoming
-	echo "Default allowing outgoing traffing"
+	echo "- Default allowing outgoing traffing"
 	#ufw default allow outgoing
-	echo "Only allowing SSH on local IPs"
+	echo "- Only allowing SSH on local IPs"
 	#ufw allow from 192.168.1.0/24 to any port 22
 
-	ufw_regex="^[0-9]{1,5}$"
-	
+	port_regex="^[0-9]{1,5}$"
+	protocol_regex="^[t|u]{1}$"
+	ufw_port=25565
+	ufw_protocol='t'	
+
 	# Ensure user input is numeric and within valid port ranges [0-65535]
-	while [ -z $ufw_port ] ; do
-		read -p "Input desired port to allow: " ufw_port
-		[[ $ufw_port =~ $ufw_regex ]] && [ $ufw_port -lt 65536 ] && [ $ufw_port -ge 0 ] && echo "hi"
+	while ! [ -z $ufw_port ] && ! [ -z $ufw_protocol ]; do
+		read -p "Input desired port to allow: " ufw_port && read -p "TCP or UDP? (t/u): " ufw_protocol
+		[[ $ufw_port =~ $port_regex ]] && [ $ufw_port -lt 65536 ] && [ $ufw_port -ge 0 ] && [[ $ufw_protocol =~ $protocol_regex ]] && echo "Valid port and protocol. Implementing."
 	done
 else
 	echo "ufw could not be found on this system. Try 'apt-get install ufw' or respective command for your distro"
